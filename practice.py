79d05dbf9cc135c289c966a85524fb5efe93688b
Mitali <mitaliibisht111@gmail.com>
2026-09-24 19:12:52 +0530

    Add practice

--- practice.py
+++ practice.py
@@ -17,3 +17,53 @@ print(df)
 print("\nColumns:", df.columns.tolist())
 print("\naverage of age:", df["Age"].mean())
 print("\nstudents lives in delhi:\n", df[df["City"] == "Delhi"])
+
+
+import pandas as pd
+data =[10,20,30,40,50]
+s=pd.Series(data)
+print(s)
+
+
+import pandas as pd
+data =[100,50,80,60]
+fruits=["Apple","Banana","Mango","Orange"]
+s=pd.Series(data,index=fruits)
+print(s)
+
+
+
+
+import pandas as pd
+data = {
+"Name":["Rahul","priya","Aman"],
+"Age":[20,21,19],
+"Marks":[85,92,78]
+
+}
+df =pd.DataFrame(data)
+print(df)
+
+
+
+#1.Number of rows and columns
+print(df.shape)
+
+#2.Column names
+print(df.columns)
+
+#3.Data types
+print(df.dtypes)
+
+#4.First 2 rows
+print(df.head(2))
+
+
+import pandas as pd
+df = pd.DataFrame({
+ "Name":["Rahul","priya","Aman"],
+ "Age":[20,21,19],
+"Marks":[85,92,78]
+})
+
+df["Marks"]
